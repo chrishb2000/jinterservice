32a23fd5e4607d89f5c7a21c656fc62c9e75992a
Christian <chbagatulj@hotmail.com>
2021-12-30 23:36:25 +0100

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+
+
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+})(jQuery); // End of use strict
